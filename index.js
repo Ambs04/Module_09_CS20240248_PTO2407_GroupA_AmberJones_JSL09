5719c48d9b8778f495214e57bcd6dab8132d5297
Ambs04 <amberleejones2007@gmail.com>
2025-02-01 19:43:28 +0200

    create base template for fetch to get info about ethereum from the API

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//SECTION 1 - BACKGROUND
+
 //Get photo from Unsplash API
 //use scrimba api - https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=pastel
 
@@ -24,3 +26,10 @@ fetch(
     //set a defaullt image if the promis eis rejected
     document.body.style.backgroundImage = `url('https://images.unsplash.com/photo-1516718674638-5dd513524b36?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3Mzg0MTY1MTJ8&ixlib=rb-4.0.3&q=80&w=1080')`;
   });
+
+//SECTION 2 - CRYPTO INFO
+
+//fetch data from the coingecko API
+fetch("https://api.coingecko.com/api/v3/coins/ethereum", { method: "GET" })
+  .then((res) => res.json())
+  .then((data) => console.log(data));
